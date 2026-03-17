5ef31a58a5d49b1154133c091c2b383c0eb1b576
ragnar-vallhala <ashutoshvishwakarma208@outlook.com>
2026-03-18 01:20:13 +0530

    chore: Increase preallocated logging file sizes from 1MB to 10MB.

--- include/variables.h
+++ include/variables.h
@@ -66,9 +66,9 @@ extern MutexHandle_t g_comm_mutex;
 #define ENABLE_BINARY_NAVLINK_PKT 0
 #define ENABLE_BINARY_NAVLINK_PKT_LOGGING 1
 #define NAVLINK_LOGGING_FILENAME "0:v_nav.bin"
-#define NAVLINK_LOGGING_FILE_SIZE 1024 * 1024 * 1 // 1MB Preallocated
+#define NAVLINK_LOGGING_FILE_SIZE 1024 * 1024 * 10 // 10MB Preallocated
 #define SYS_LOGGING_FILENAME "0:v_sys.bin"
-#define SYS_LOGGING_FILE_SIZE 1024 * 1024 * 1 // 1MB Preallocated
+#define SYS_LOGGING_FILE_SIZE 1024 * 1024 * 10 // 10MB Preallocated
 #define GENERAL_LOGGING_FILENAME "0:v_gen.bin"
-#define GENERAL_LOGGING_FILE_SIZE 1024 * 1024 * 1 // 1MB Preallocated
+#define GENERAL_LOGGING_FILE_SIZE 1024 * 1024 * 10 // 10MB Preallocated
 #endif //! VAYU_VARIABLES_H
