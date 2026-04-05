7d084035dd276772ce97941b462c3d7136d1579b
ragnar-vallhala <ashutoshvishwakarma208@outlook.com>
2026-04-05 16:28:46 +0530

    feat: implement outer-loop angle control and tune PID gains for improved flight stability

--- include/maths/control.h
+++ include/maths/control.h
@@ -6,30 +6,35 @@
 #include "maths/lpf.h"
 
 // Angle Loop Gains
-#define PID_ROLL_ANGLE_KP 3.0f
+#define PID_ROLL_ANGLE_KP 1.0f
 #define PID_ROLL_ANGLE_KI 0.0f
 #define PID_ROLL_ANGLE_KD 0.0f
 #define PID_ROLL_ANGLE_I_LIMIT 0.0f
 
-#define PID_PITCH_ANGLE_KP 3.0f
+#define PID_PITCH_ANGLE_KP 1.0f
 #define PID_PITCH_ANGLE_KI 0.0f
 #define PID_PITCH_ANGLE_KD 0.0f
 #define PID_PITCH_ANGLE_I_LIMIT 0.0f
 
+#define PID_YAW_ANGLE_KP 1.0f
+#define PID_YAW_ANGLE_KI 0.0f
+#define PID_YAW_ANGLE_KD 0.0f
+#define PID_YAW_ANGLE_I_LIMIT 0.0f
+
 // Rate Loop Gains
-#define PID_ROLL_RATE_KP 0.1f
-#define PID_ROLL_RATE_KI 0.02f
-#define PID_ROLL_RATE_KD 0.01f
+#define PID_ROLL_RATE_KP 0.12f
+#define PID_ROLL_RATE_KI 0.00f
+#define PID_ROLL_RATE_KD 0.00f
 #define PID_ROLL_RATE_KD_LPF_ALPHA 0.7f
-#define PID_ROLL_RATE_I_LIMIT 0.5f
+#define PID_ROLL_RATE_I_LIMIT 0.2f
 #define PID_ROLL_RATE_OUT_LIMIT 0.3f
 #define PID_ROLL_RATE_EXPO 0.7f
 
-#define PID_PITCH_RATE_KP 0.06f
-#define PID_PITCH_RATE_KI 0.02f
-#define PID_PITCH_RATE_KD 0.008f
-#define PID_PITCH_RATE_KD_LPF_ALPHA 0.1f
-#define PID_PITCH_RATE_I_LIMIT 0.5f
+#define PID_PITCH_RATE_KP 0.12f
+#define PID_PITCH_RATE_KI 0.00f
+#define PID_PITCH_RATE_KD 0.00f
+#define PID_PITCH_RATE_KD_LPF_ALPHA 0.7f
+#define PID_PITCH_RATE_I_LIMIT 0.2f
 #define PID_PITCH_RATE_OUT_LIMIT 0.3f
 #define PID_PITCH_RATE_EXPO 0.7f
 
@@ -43,7 +48,7 @@
 #define PID_YAW_RATE_EXPO 0.7f
 
 // Control Limits
-#define MAX_CONTROL_ANGLE 30.0f  // max tilt in degrees
+#define MAX_CONTROL_ANGLE 40.0f  // max tilt in degrees
 #define MAX_CONTROL_RATE 40.0f  // max target rate in deg/s
 #define MOTOR_MIN_THROTTLE 0.05f // minimum for armed motors
 
